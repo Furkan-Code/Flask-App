0eda94c125f4fce75678ff13a35cbe86eba7848f
Furkan Gökmen <111159462+Furkan-Code@users.noreply.github.com>
2024-05-20 19:41:16 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def index():
-    return render_template('index.html')
+def start():
+    return "The MBSA Server is running"
+@app.route('/mbsa')
 
+def mbsa():
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
