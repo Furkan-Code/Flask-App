ce7fadc4c17ce1a0dd575fe3cc1b210691731569
Furkan Gökmen <111159462+Furkan-Code@users.noreply.github.com>
2024-05-20 21:05:58 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
 
 app = Flask(__name__)
+
+model_path = "C:\\Users\\Furkan\\Desktop\\content\\outputs"
+tokenizer_path = "C:\\Users\\Furkan\\Desktop\\content\\outputs"
+# Model ve tokenizer yükleniyor
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
 
 @app.route('/')
 
@@ -10,5 +19,17 @@ def start():
 
 def mbsa():
     return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+
+def analyze():
+    data = request.get_json()
+    text = data['text']
+    # Metni sınıflandırıp sonucu döndür
+    result = nlp(text)
+    # Sonucun içerisinde belirli bir afet türünü arayarak 'yes' veya 'no' döndür
+    response = 'yes' if result[0]['label'] == 'LABEL_0' else 'no'
+    return jsonify({'prediction': response})
+
 if __name__ == '__main__':
     app.run(debug=True)
